7e2af33ea24e3bc2850f008465e45ba8fee2c39f
ynxie <36654446+ynxie@users.noreply.github.com>
2018-09-23 21:17:33 -0400

    Add files via upload

--- EC601_miniproject1.py
+++ EC601_miniproject1.py
@@ -1,9 +1,4 @@
 
-# coding: utf-8
-
-# In[4]:
-
-
 #Author：Yuning Xie
 #Professor: Osama
 #Course: EC601
@@ -19,7 +14,7 @@ import os
 from google.cloud import vision
 from google.cloud.vision import types
 import glob
-import cv2
+from PIL import Image, ImageFont, ImageDraw
 
 #Twitter API credentials
 consumer_key = "input your keys"
@@ -28,9 +23,7 @@ access_key = "input your keys"
 access_secret = "input your keys"
 
 def get_all_tweets(screen_name):
-    
     #Twitter only allows access to a users most recent 3240 tweets with this method
-    
     #authorize twitter, initialize tweepy
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
@@ -40,7 +33,7 @@ def get_all_tweets(screen_name):
     alltweets = []    
     
     #make initial request for most recent tweets (200 is the maximum allowed count)
-    new_tweets = api.user_timeline(screen_name = screen_name,count=10)
+    new_tweets = api.user_timeline(screen_name = screen_name,count = 10)
     
     #save most recent tweets
     alltweets.extend(new_tweets)
@@ -52,7 +45,7 @@ def get_all_tweets(screen_name):
     while len(new_tweets) > 0:
         
         #all subsiquent requests use the max_id param to prevent duplicates
-        new_tweets = api.user_timeline(screen_name = screen_name,count=10,max_id=oldest)
+        new_tweets = api.user_timeline(screen_name = screen_name,count = 10,max_id = oldest)
         
         #save most recent tweets
         alltweets.extend(new_tweets)
@@ -62,15 +55,14 @@ def get_all_tweets(screen_name):
         if(len(alltweets) > 15):
             break
         print ("...%s tweets downloaded so far" % (len(alltweets)))
-       
+
     #write tweet objects to JSON
     file = open('tweet.json', 'w') 
-    print ("Writing tweet objects to JSON please wait...")
+    #print ("Writing tweet objects to JSON please wait...")
     for status in alltweets:
         json.dump(status._json,file,sort_keys = True,indent = 4)
-    
     #close the file
-    print ("Done")
+    #print ("Done")
     file.close()
     
     #get images url
@@ -81,50 +73,75 @@ def get_all_tweets(screen_name):
             media_files.add(media[0]['media_url'])
             
     #download the images
-    print("Downloading images please wait...")
-    i = 1
-    for media_file in media_files:
-        wget.download(media_file,'image'+str(i)+'.jpg')
-        i = i + 1
-    
+    if(len(media_files) > 0):
+        print("Downloading images please wait...")
+        i = 1
+        for media_file in media_files:
+            wget.download(media_file,'image'+str(i)+'.jpg')
+            i = i + 1
+        print("Done")
+    else:
+        print("No image in this twitter account.")
+    return(media_files)
+             
+def convert_to_video(screen_name):
     #change images into video
-    print("Changing images into video please wait...")
-    os.system('ffmpeg -framerate 2 -i image%d.jpg -r 15 video.mp4')
-    print("Done")
+    media_files = get_all_tweets(screen_name)
+    if(len(media_files) > 0):
+        print("Changing images into video please wait...")
+        os.system('ffmpeg -framerate 2 -i image%d.jpg -r 15 video.mp4')
+        print("Done")
+    else:
+        print("Cannot create a valid video.")
     
+def analysis_content(screen_name):    
     #google vision analysis
     #Google API credentials
-    print("Analysising the content please wait...")
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:\\Users\\xieyn\Downloads\\My First Project-a6794e647960.json"
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "input the path of json file"
     client = vision.ImageAnnotatorClient()
 
     img_path = glob.glob('*.jpg') 
-    i = 1
-    for i in range(len(img_path)):
-    # The name of the image file to annotate
-        file_name = os.path.join(
-            os.path.dirname('__file__'),
-            img_path[i])
+    if(len(img_path) > 0):
+        print("Analysing the content please wait...")
+        i = 1
+        for i in range(len(img_path)):
+        # The name of the image file to annotate
+            file_name = os.path.join(
+                        os.path.dirname('__file__'),
+                        img_path[i])
+        # Loads the image into memory
+            with io.open(file_name, 'rb') as image_file:
+                content = image_file.read()
 
-    # Loads the image into memory
-        with io.open(file_name, 'rb') as image_file:
-            content = image_file.read()
+            image = types.Image(content = content)
 
-        image = types.Image(content = content)
+        # Performs label detection on the image file
+            response = client.label_detection(image = image)
+            labels = response.label_annotations
 
-    # Performs label detection on the image file
-        response = client.label_detection(image = image)
-        labels = response.label_annotations
+        #tag the description on the image
+            im = Image.open(file_name)
+            draw = ImageDraw.Draw(im)
+            font = ImageFont.truetype("C:\Windows\Fonts\simsunb.ttf",48)
 
-        print('Image Name:'+ img_path[i])
+            a= 10
+            b = 10
+            for label in labels:
+                draw.text((a,b),label.description, fill = (255, 0, 0), font = font)
+                b = b + 40
+                im.save('new_image' + str(i + 1) + '.jpg')
+            i = i + 1
+            
+        os.system('ffmpeg -framerate 2 -i new_image%d.jpg -r 15 new_video.mp4')
+        print("Done")
+    else:
+        print("No content to analyse.")
 
-        print('Labels:')
-        for label in labels:
-            print(label.description)
-
-        i = i + 1
-    
+        
 if __name__ == '__main__':
     #pass in the username of the account you want to download
-    get_all_tweets("@blakelively")
+    screen_name = input("Please input a twitter account")
+    convert_to_video(screen_name)
+    analysis_content(screen_name)
+        
 
